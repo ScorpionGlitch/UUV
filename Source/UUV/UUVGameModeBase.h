3c2faddef7b8bac44cc8a0ba3dcb56a078bb6aba
David Allen Archer <David.Allen.Archer@Gmail.com>
2022-01-10 22:27:12 -0500

    initial commit of project

---
+++ Source/UUV/UUVGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UUVGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UUV_API AUUVGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
